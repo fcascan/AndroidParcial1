ddefb35a4bc3951a77c5f58b3aea2c570b5f3f33
fcastrocanosa <fcastrocanosa@asjservicios.com>
2023-04-25 23:01:25 -0300

    Initial commit (empty template)

---
+++ app/src/main/java/com/fcascan/parcial1/activities/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.fcascan.parcial1.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.fcascan.parcial1.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
